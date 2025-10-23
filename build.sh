e027be00c53476ac914024891676ccbfbf178218
boneyk <yanastya05gmail.com>
2025-10-24 00:13:52 +1000

    создание и инициализация проекта

---
+++ build.sh
@@ -0,0 +1,17 @@
+set -e
+
+echo "Обновление исходников из Git"
+if [-d.git ]; then
+    git pull origin main || echo "Репозиторий не синхронизирован с удаленным"
+else
+    echo "Git-репозиторий не инициализирован"
+fi
+
+echo "Сборка проекта"
+mkdir -p build
+cd build
+cmake ..
+make
+
+echo "Запуск программы"
+./hello
