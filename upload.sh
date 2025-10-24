bd5dc9fb08b25f27d83c96f9af9a6c3d73f9cd49
boneyk <yanastya05gmail.com>
2025-10-24 14:48:53 +1000

    Добавлен скрипт сборки

---
+++ upload.sh
@@ -0,0 +1,24 @@
+set -e
+echo "Добавление изменений в репозиторий"
+
+if [! -d.git]; then
+    echo "Локальный репозиторий отсутствует"
+    exit 1
+fi
+
+git status
+git add .
+
+if [-z "$1"]; then
+    COMMIT_MSG = "Auto commit"
+else
+    COMMIT_MSG="$1"
+fi
+
+git commit -m "$COMMIT_MSG" || echo "Нет изменинй для коммита"
+
+echo "Отправка изменений на гитхаб"
+
+git push origin main
+
+echo "Изменения успешно загружены"
